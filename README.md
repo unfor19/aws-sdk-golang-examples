9ea3cd7e50e74a56746d2a1e38fdb63ce775efb8
Meir Gabay <unfor19@gmail.com>
2021-02-03 01:25:11 +0200

    full example

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # aws-sdk-golang-examples
+
+An example of how to use AWS SDK Resource Groups Tagging API (resourcegroupstaggingapi) in GoLang.
+
+Tested on [WSL2](https://docs.microsoft.com/en-us/windows/wsl/install-win10) Ubuntu 20.04
+
+## Requirements
+
+- [golang v1.15](https://golang.org/doc/install)
+- [AWS Credentials](https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html)
+- (Optional) [Visual Studio Code](https://code.visualstudio.com/download) and its [Go extension](https://marketplace.visualstudio.com/items?itemName=golang.Go)
+
+## Installation
+
+1. Prepare your terminal's ~/.rc file (`.bashrc`, `.bash_profile`, `.zshrc`, etc.)
+
+    ```bash
+    # GOLANG
+    export GOPATH="$HOME/go"
+    export GOBIN="/usr/local/go/bin"
+    export PATH="$PATH:$GOPATH:$GOBIN"
+    # export GO111MODULE=on # IMPORTANT: If you see this line, comment it out
+    ```
+
+1. Reload the rc file - `source ~/.bashrc`
+1. Just in case - `unset GO111MODULE`
+1. Create a directory in `${HOME}/go/`
+    ```bash
+    $ TARGET_DIR="${HOME}/go/src/internal/aws-sdk-golang-resourcegroupstaggingapi"
+    $ mkdir -p "$TARGET_DIR"
+    ```
+1. Why `/internal/`? Using the reserved keyword `internal` as a directory name instructs `go get` to consider all sub-directories as [internal packages](https://golang.org/doc/go1.4#internalpackages)
+
+## Usage
+
+```bash
+# Download and install dependencies
+$ TARGET_DIR="${HOME}/go/src/internal/aws-sdk-golang-resourcegroupstaggingapi"
+$ cd "$TARGET_DIR"
+$ cp aws-sdk-golang-resourcegroupstaggingapi.go "$TARGET_DIR"/
+$ go get -u -v # -u = download latest, -v = verbose output
+
+# `go get` Also builds a binary and copies it to /usr/local/go/bin/aws-sdk-golang-examples
+# TODO: figure out why
+
+# Edit aws-sdk-golang-resourcegroupstaggingapi.go
+# Set the relevant region and filters
+
+# Run the application from source code
+$ go run .
+
+# Build the application and run the binary
+$ go build .
+$ ./aws-sdk-golang-examples
+
+# Check the size of the binary file
+$ du -h aws-sdk-golang-examples # du = disk usage, -h = human readable
+8.8M
+```
+
+
+## Troubleshooting
+
+1. `go get` permission denied to `/usr/local/go/bin` directory
+
+    **Error**
+    ```bash
+    go get internal/aws-sdk-golang-examples: copying /tmp/go-build594918589/b001/exe/a.out: open /usr/local/go/bin/aws-sdk-golang-examples: permission denied
+    ```
+
+    **Fix**
+    ```bash
+    # Set current user:user_group as the owner
+    $ sudo chown -R $USER:$(id -g -n $USER) /usr/local/go/bin/
+    ```
---
+++ main.go
@@ -0,0 +1,86 @@
+/*
+### Resource Groups Tagging API
+GoLang SDK Docs: https://docs.aws.amazon.com/sdk-for-go/api/service/resourcegroupstaggingapi/
+The package resourcegroupstaggingapi provides the client and types for making API requests to AWS Resource Groups Tagging API.
+This example was written especially for GoLang newcomers (like me).
+Scenario: Getting all resources ARNs which are tagged with "APP_NAME = api-group"
+Given: There are two resources that are tagged with "APP_NAME=api-group", both are S3 buckets
+###
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi"
+	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi/types"
+)
+
+func main() {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("eu-west-1"),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Variables that will be used for querying
+	tagKey := aws.String("APP_NAME")
+	tagValues := []string{
+		"api-group",
+	}
+
+	// Define a tagFilter
+	tagFilter := types.TagFilter{}
+	tagFilter.Key = tagKey
+	tagFilter.Values = tagValues
+
+	// Using the Config value, create the DynamoDB client
+	svc := resourcegroupstaggingapi.NewFromConfig(cfg)
+	params := &resourcegroupstaggingapi.GetResourcesInput{
+		TagFilters: []types.TagFilter{
+			// {Key: aws.String("APP_NAME"), Values: []string{"group-api"}}, // Inline values
+			// {Key: tagKey, Values: tagValues}, // Variables references
+			tagFilter, // Variable reference to an object
+		},
+	}
+
+	resp, err := svc.GetResources(context.TODO(), params)
+	// Build the request with its input parameters
+	if err != nil {
+		log.Fatalf("failed to list resources, %v", err)
+	}
+
+	for _, res := range resp.ResourceTagMappingList {
+		fmt.Println("      Pointer address:", res.ResourceARN)
+		fmt.Println(" Value behind pointer:", *res.ResourceARN)
+		fmt.Println("                Value:", aws.ToString(res.ResourceARN))
+		fmt.Println()
+	}
+
+	// Use value
+	random_item_index := rand.Intn(len(resp.ResourceTagMappingList))
+	s := "      Random Resource: " + *resp.ResourceTagMappingList[random_item_index].ResourceARN
+	fmt.Println(s)
+}
+
+/*
+### Output
+      Pointer address: 0xc000393c70
+ Value behind pointer: arn:aws:s3:::meir-test-1-api
+                Value: arn:aws:s3:::meir-test-1-api
+      Pointer address: 0xc000393cd0
+ Value behind pointer: arn:aws:s3:::meir-test-2-api
+                Value: arn:aws:s3:::meir-test-2-api
+      Random Resource: arn:aws:s3:::meir-test-2-api
+###
+*/
